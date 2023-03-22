34d88ab7a1e6aa0d416ffd54726336a16b5f69ed
elaine2700 <easerrano.e@gmail.com>
2023-03-21 22:13:56 -0700

    Created Quiz

---
+++ Quiz_App/Models/Quiz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Quiz_App.Models
+{
+    public class Quiz
+    {
+        public List<Question>? questions { get; private set; }
+        public string? Title { get; set; }
+        public int currentQuestion { get; set; }
+        public int correctAnswers { get; set; }
+        
+        public Quiz()
+		{
+            questions = new List<Question>();
+            currentQuestion = 0;
+            correctAnswers = 0;
+        }
+
+		public void AddQuestion(Question _question)
+		{
+            if (questions == null)
+                questions = new List<Question>();
+            questions.Add(_question);
+		}
+
+        public double CalculateScore()
+        {
+            if (questions == null) return 0;
+            return (correctAnswers * 100) / questions.Count;
+        }
+
+	}
+}
+
---
+++ Quiz_App/Pages/QuizPage.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Quiz_App.Models;
+
+namespace Quiz_App.Pages
+{
+    public class QuizPageModel : PageModel
+    {
+        public Quiz? quiz { get; set; } 
+
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
